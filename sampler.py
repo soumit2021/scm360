5a405554223441996f89b6a3dcd9f2da91de62d2
soumit2021 <soumit123@gmail.com>
2023-05-15 08:40:31 +0600

    sampler class

--- sampler.py
+++ sampler.py
@@ -12,3 +12,40 @@ def read_data(date, instrument, time_step):
 
 
 
+class Sampler:
+
+	def load_db(self, fld):
+
+		self.db = pickle.load(open(os.path.join(fld, 'db.pickle'),'rb'))
+		param = json.load(open(os.path.join(fld, 'param.json'),'rb'))
+		self.i_db = 0
+		self.n_db = param['n_episodes']
+		self.sample = self.__sample_db
+		for attr in param:
+			if hasattr(self, attr):
+				setattr(self, attr, param[attr])
+		self.title = 'DB_'+param['title']
+
+
+	def build_db(self, n_episodes, fld):
+		db = []
+		for i in range(n_episodes):
+			prices, title = self.sample()
+			db.append((prices, '[%i]_'%i+title))
+		os.makedirs(fld)
+		pickle.dump(db, open(os.path.join(fld, 'db.pickle'),'wb'))
+		param = {'n_episodes':n_episodes}
+		for k in self.attrs:
+			param[k] = getattr(self, k)
+		json.dump(param, open(os.path.join(fld, 'param.json'),'w'))
+
+
+	def __sample_db(self):
+		prices, title = self.db[self.i_db]
+		self.i_db += 1
+		if self.i_db == self.n_db:
+			self.i_db = 0
+		return prices, title
+
+
+
